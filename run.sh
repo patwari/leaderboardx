8c4ef7ca3c2ef36806ffbced40705dade7168b74
patwari <rahulpatwari233@gmail.com>
2025-12-31 13:07:48 +0530

    Initialize alembic for database migrations

--- run.sh
+++ run.sh
@@ -8,7 +8,8 @@ echo "Building and starting LeaderboardX..."
 # Build and run with `docker compose`
 docker compose down
 docker compose build
-docker compose up --remove-orphans
+# docker compose up --remove-orphans
+docker compose up --detach --remove-orphans
 
 echo "LeaderboardX is running on http://localhost:8000"
 echo "health is running on http://localhost:8000/health"
